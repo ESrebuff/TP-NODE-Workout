f58d51e833c4016b891b58d096110d69d21d239b
Esteban <estebanianis@gmail.com>
2021-11-03 10:03:49 +0100

    Tp-js

--- models/WorkoutExo.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const workoutExoSchema = new mongoose.Schema({
-    name: String,
-    date: { type: Date, default: Date.now }
-})
-
-export const WorkoutExos = mongoose.model('WorkoutExos', workoutExoSchema)
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { set_account, connect_account} from '../controllers/userController.js'
-const router = express.Router()
-
-router.post('/register', set_account)
-router.post('/connect', connect_account)
-
-export default router
